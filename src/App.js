1afcbfdec8ac5d23c3d98181fe2cfb224dbb98b2
Tales Godois <tales.godois@gmail.com>
2019-06-06 21:49:19 -0300

    starting with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        inputValue: ''
+    };
+
+    inputChange = event => {
+        this.setState({
+            inputValue: event.target.value
+        });
+    };
+
+    render() {
+        const { newValue, clickButton } = this.props;
+        const { inputValue } = this.state;
+        return (
+            <div
+                className="App"
+                type="text"
+                value={inputValue}
+                style={{ paddingTop: '10px' }}
+                onChange={this.inputChange}
+            >
+                <input type="text" />
+                <button onClick={() => clickButton(inputValue)}>
+                    Click me!
+                </button>
+                <h1>{newValue}</h1>
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = store => ({
+    newValue: store.clickState.newValue
+});
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ clickButton }, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
